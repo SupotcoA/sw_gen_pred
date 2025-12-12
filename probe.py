1a5f38d9be6fa3323ad7092144761b659d2625c1
SupotcoA <152614457+SupotcoA@users.noreply.github.com>
2025-12-12 13:19:46 +0800

    fix

--- probe.py
+++ probe.py
@@ -120,7 +120,7 @@ def diff_loss(model, dataset, logger, num_test_steps=50, metric_idx=0):
     colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
     
     for i, (ds,c) in enumerate(zip(diff_steps,colors)):
-        plt.errorbar(np.arange(len(ls_m.shape[0])), ls_m[i], color=c+"80", ecolor=c+"30", yerr=std_loss[i], fmt='-o', capsize=5, label=f'{ds} steps')
+        plt.errorbar(np.arange(ls_m.shape[1]), ls_m[i], color=c+"80", ecolor=c+"30", yerr=ls_s[i], fmt='-o', capsize=5, label=f'{ds} steps')
         # window_size = 15
         # if len(mean_loss[i]) >= window_size:
         #     # pad the sequence to avoid losing points at the edges
